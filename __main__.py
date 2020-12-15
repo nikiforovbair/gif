cab03cdb9804e5522710571adce457a919b0400a
nikiforovbair <nikiforovbair@gmail.com>
2020-12-15 03:45:57 +0300

    add func open dir

--- __main__.py
+++ __main__.py
@@ -16,7 +16,7 @@ def check_int_and_sort(list_files):
     list_sort_files.sort()
     return list_sort_files
 
-def create_images_is_binary_files_ct(_path, list_files):
+def create_images_is_binary_files_ct(_path, list_files, color_module):
     SPECTR_UINT = 4096 / 255
     DIFF = 1024 / SPECTR_UINT
     
@@ -29,31 +29,39 @@ def create_images_is_binary_files_ct(_path, list_files):
         data_ct = data_ct / SPECTR_UINT
         data_ct = data_ct + DIFF
         
-        image = Image.fromarray(np.uint8(data_ct), 'L')
+        image = Image.fromarray(np.uint8(data_ct), 'L').convert(color_module)
         frames.append(image)
         print(file)
     return frames
 
 
-def main(): 
+def main():
     print("Укажите путь к файлам (например: C:\ct-pickle-dumps\\" + "1\\")
     _path = input()
+    print("Укажите цветовой модуль (например: RGB, RGBA, L - grayscale")
+    color_module = input()
 
     start_time = time.time()
 
+    for dirpath, dirnames, filenames in os.walk(_path):
+        list_files = filenames
+        list_sort_files = check_int_and_sort(list_files)
+        
+        if (len(list_sort_files) == 0):
+            continue
 
-    list_files = os.listdir(path=_path)
-    list_sort_files = check_int_and_sort(list_files)
+        frames = create_images_is_binary_files_ct(dirpath, list_sort_files, color_module)
 
-    frames = create_images_is_binary_files_ct(_path, list_sort_files)
+        frames[0].save(str(dirpath) + "_gif_result.gif", format = 'GIF',
+            append_images = frames[1:],
+            save_all = True, Loop = 0)
+    
 
-    frames[0].save('gif_result.gif', format = 'GIF',
-                   append_images = frames[1:],
-                   save_all = True, Loop = 0)
-
-    print("Save file: gif_result.gif " + "End time: " +"--- %s seconds ---" % (time.time() - start_time))
-    print(input())
+    print("Save files. " + "End time: " +"--- %s seconds ---" % (time.time() - start_time))
+    input()
 
 if __name__ == "__main__":
     main()
+    pass
+
 
